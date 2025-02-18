67c391c44f6fb173377e3e92f9f5d2c1343b28a2
mmsfarsi <mirmdsalmanfarsi.1@gmail.com>
2025-02-18 23:42:52 +0600

    welcome section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import HealthTips from './HealthTips'
 import PopularCamps from './PopularCamps'
 import Reviews from './Reviews'
 import Ads from './Ads'
+import Welcome from './Welcome'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
     <div className='bg-[#FCF8F8]'>
       <Banner></Banner>
       <Ads></Ads>
+      <Welcome></Welcome>
       <PopularCamps></PopularCamps>
       <Reviews></Reviews>
       <Promotion></Promotion>
---
+++ src/Pages/Home/Welcome.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 items-center p-6">
+      <div className="overflow-hidden">
+        <img  src="https://i.ibb.co.com/qF3HvTt9/doctors.jpg"  alt="Medical Team"   className="w-full h-full object-cover  "/>
+      </div>
+
+      <div className="text-center md:text-left">
+        <h1 className="text-lg text-gray-600  ">
+          Welcome to Lifelines Camp
+        </h1>
+        <p className="text-2xl md:text-3xl text-gray-800 font-bold mt-2">
+          Providing top-quality medical care with expert doctors and advanced healthcare solutions.
+        </p>
+
+        <h2 className="text-xl font-semibold text-[#B354A6] mt-4">   Dr. Saiful Islam Saif   </h2>
+        <p className="text-md text-gray-500">Expert in Surgery & Critical Care</p>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
